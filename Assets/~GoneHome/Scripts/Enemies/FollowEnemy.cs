84979fba645a5b21c10f78ef44d10f2721855b0b
teacher <teacher@600900B211DE415>
2018-03-15 13:35:27 +1100

    Added AI

---
+++ Assets/~GoneHome/Scripts/Enemies/FollowEnemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.AI;
+// Clean code: CTRL + K + D (In that order)
+
+namespace GoneHome
+{
+    public class FollowEnemy : MonoBehaviour
+    {
+        public Transform target;
+
+        private NavMeshAgent agent;
+        
+        private Vector3 spawnPoint;
+
+        // Use this for initialization
+        void Start()
+        {
+            // Making a copy of the starting position
+            spawnPoint = transform.position;
+
+            agent = GetComponent<NavMeshAgent>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            agent.SetDestination(target.position);
+        }
+
+        public void Reset()
+        {
+            // Reset position of player to starting position
+            transform.position = spawnPoint;   
+        }
+    }
+}
